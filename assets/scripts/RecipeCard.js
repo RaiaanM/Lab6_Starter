349197864bf5e9b12e324e190eee45d37f6c80a1
Raiaan Mortuja <107028751+RaiaanM@users.noreply.github.com>
2022-11-07 14:41:41 -0800

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,12 +8,19 @@ class RecipeCard extends HTMLElement {
 
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
+    
     let shadowA = this.attachShadow({mode: 'open'});
+    
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
-     let articleA = document.createElement('article');
+    
+    let articleA = document.createElement('article');
+    
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
+    
     let styleA = document.createElement('style');
+    
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
+    
     styleA.textContent = `
     * {
       font-family: sans-serif;
@@ -91,6 +98,7 @@ class RecipeCard extends HTMLElement {
       font-size: 12px;
     }`
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
+    
     shadowA.appendChild(styleA);
     shadowA.appendChild(articleA);
   }
@@ -121,11 +129,14 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+    
     let articleA = this.shadowRoot.querySelector('article');
+    
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
+    
     articleA.innerHTML = 
     `<img src=${data.imgSrc} alt=${data.imgSrc}>
     <p class="title">
@@ -146,3 +157,5 @@ class RecipeCard extends HTMLElement {
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
+
+customElements.define("recipe-card", RecipeCard);
